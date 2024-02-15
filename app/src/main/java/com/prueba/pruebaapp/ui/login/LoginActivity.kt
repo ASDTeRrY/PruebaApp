8014ff38fc59fca547e4301add16edac3b80d998
ASD.sLyon <cesar.rodriguezl@hotmail.com>
2024-02-15 18:29:56 -0500

    firts commin
    create proyect and clean arquitecture structure

---
+++ app/src/main/java/com/prueba/pruebaapp/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.prueba.pruebaapp.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.prueba.pruebaapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
