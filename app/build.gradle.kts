4bb3a00265a96cd79ded742d8700896a048d015e
ASD.sLyon <cesar.rodriguezl@hotmail.com>
2024-02-15 19:06:05 -0500

    add navigation
    n

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,4 +47,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //NavComponent
+    val navVersion = "2.7.7"
+    implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 }
--- app/src/main/java/com/prueba/pruebaapp/ui/login/LoginActivity.kt
+++ app/src/main/java/com/prueba/pruebaapp/ui/login/LoginActivity.kt
@@ -3,10 +3,15 @@ package com.prueba.pruebaapp.ui.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.prueba.pruebaapp.R
+import com.prueba.pruebaapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
